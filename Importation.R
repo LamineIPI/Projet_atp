02e54dea7ba5ddc3c91bf004ee4dcd86e48445c5
Lamine Gueye <gueyelamine076@gmail.com>
2020-10-22 22:23:31 +0200

    Importation des données

---
+++ Importation.R
@@ -0,0 +1,22 @@
+Importation de fichiers
+setwd("C:/Users/uc/Desktop/Master SEP/Tests Stat/tennis_atp-master")
+
+# Packages
+library(tidyverse)
+library("stringr")
+library('purrr')
+### Importation de la atp_players contenant les identifiants des joueurs
+players <- read_csv(file = "atp_players.csv",
+                    col_names = FALSE)
+names(players) <- c("id", "firstname", "lastname", "hand", "birthday", "nat")
+
+
+### Importation des fichiers des matchs atp joués de 1968 à 2020
+lst <- list.files(path = "C:/Users/uc/Desktop/Master SEP/Tests Stat/tennis_atp-master")
+lst_data <- grep(pattern = "^atp_matches_[[:digit:]]{4}.csv$", x = lst, value = TRUE)
+lst_names <- paste('atp', str_extract(string = lst_data, pattern = "[[:digit:]]{4}"), sep = "")
+lst_tib <- map(.x = lst_data, function (x) read_csv(paste("C:/Users/uc/Desktop/Master SEP/Tests Stat/tennis_atp-master/", x, sep = "")))
+names(lst_tib) <- lst_names
+
+atp <- reduce(.x = lst_tib, .f = bind_rows)
+head(atp)
